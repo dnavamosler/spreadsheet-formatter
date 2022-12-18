2ed82db2afc295b1de96d331bed5058f86c8f473
Daniel Nava <dnavamoslercl@gmail.com>
2022-12-18 19:30:37 -0300

    fix

---
+++ app/index.d.ts
@@ -0,0 +1,6 @@
+declare module 'spreadsheet-formatter' {
+    export function setConfig (secret: string,account:string);
+    export function getSpreadSheet (spreadSheetId: string,metadataId:string);
+    export function getPageSpreadSheet (spreadSheetId: string,pageTitle:string,pageId:string,asObject?:boolean);
+
+};
